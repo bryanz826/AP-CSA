e67159e785d2e2ba4e087e94b119050860f6a600
bryanz826 <bryanz826@gmail.com>
2018-02-08 09:10:32 -0800

    Github Test
    Unit 0

---
+++ Unit0/src/Lab0a3.java
@@ -0,0 +1,29 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Bryan Zhang
+//Date - 2018/02/02
+//Class - APCSA
+//Lab  - 0a3
+
+public class Lab0a3
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Bryan \t  2/1/2018 \t Period 2\n\n" );
+		System.out.println("This is a ship animal!" );
+		System.out.println("\n\n\n\n" );
+
+
+		System.out.println("                    /|          _              ");
+		System.out.println("                   | |________ / |             ");
+		System.out.println("         ____ '--|_|/__/__|__|_____            ");
+		System.out.println("___---__/    |__/_/_/_/__|-------______________");
+		System.out.println("|                                             /");
+		System.out.println(" ____________________________________________| ");
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" );
+	}
+}
---
+++ Unit0/src/Lab0b.java
@@ -0,0 +1,62 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Bryan Zhang
+//Date - 2018/02/02
+//Class - APCSA
+//Lab  - 0b
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 221;
+		int intOne = 321;
+		long longOne = 4646;
+		
+		//decimal variables
+		float floatOne = 33.27f;
+		double doubleOne = 3.2225;
+
+		//other integer types
+		char charOne = 'C';
+
+		//other types
+		boolean booleanOne = true;
+		String String1 = "asdf";
+		
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Bryan Zhang            02/01/18*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*	  real types		*");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*	   other types		*");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+byteOne+"\t\t*");
+		System.out.println("*stringOne = "+byteOne+"\t\t*");
+		System.out.println("'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'");
+
+	}
+}
