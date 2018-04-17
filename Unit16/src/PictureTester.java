10c8f368019bd4f03de4b2c267c915a383bafad5
bryanz826 <bryanz826@gmail.com>
2018-04-16 20:34:40 -0700

    part of Unit16

--- Unit16/src/PictureTester.java
+++ Unit16/src/PictureTester.java
@@ -58,27 +58,76 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    testZeroBlue();
-    //testKeepOnlyBlue();
-    //testKeepOnlyRed();
-    //testKeepOnlyGreen();
-    //testNegate();
-    //testGrayscale();
-    //testFixUnderwater();
-    //testMirrorVertical();
-    //testMirrorTemple();
-    //testMirrorArms();
-    //testMirrorGull();
-    //testMirrorDiagonal();
-    //testCollage();
-    //testCopy();
-    //testEdgeDetection();
-    //testEdgeDetection2();
-    //testChromakey();
-    //testEncodeAndDecode();
-    //testGetCountRedOverValue(250);
-    //testSetRedToHalfValueInTopHalf();
-    //testClearBlueOverValue(200);
-    //testGetAverageForColumn(0);
+//    testZeroBlue();
+//    testKeepOnlyBlue();
+//    testKeepOnlyRed();
+//    testKeepOnlyGreen();
+//    testNegate();
+//    testGrayscale();
+    testFixUnderwater();
+//    testMirrorVertical();
+//    testMirrorTemple();
+//    testMirrorArms();
+//    testMirrorGull();
+//    testMirrorDiagonal();
+//    testCollage();
+//    testCopy();
+//    testEdgeDetection();
+//    testEdgeDetection2();
+//    testChromakey();
+//    testEncodeAndDecode();
+//    testGetCountRedOverValue(250);
+//    testSetRedToHalfValueInTopHalf();
+//    testClearBlueOverValue(200);
+//    testGetAverageForColumn(0);
   }
+
+  	public static void testKeepOnlyBlue()
+  	{
+	    Picture beach = new Picture("beach.jpg");
+	    beach.explore();
+	    beach.keepOnlyBlue();
+	    beach.explore();
+  	}
+  	
+  	public static void testKeepOnlyRed()
+  	{
+	    Picture beach = new Picture("beach.jpg");
+	    beach.explore();
+	    beach.keepOnlyRed();
+	    beach.explore();
+  	}
+  	
+  	public static void testKeepOnlyGreen()
+  	{
+	    Picture beach = new Picture("beach.jpg");
+	    beach.explore();
+	    beach.keepOnlyGreen();
+	    beach.explore();
+  	}
+  	
+  	public static void testNegate()
+  	{
+	    Picture beach = new Picture("beach.jpg");
+	    beach.explore();
+	    beach.negate();
+	    beach.explore();
+  	}
+  	
+  	public static void testGrayscale()
+  	{
+	    Picture beach = new Picture("beach.jpg");
+	    beach.explore();
+	    beach.grayscale();
+	    beach.explore();
+  	}
+
+  	public static void testFixUnderwater()
+  	{
+	    Picture water = new Picture("water.jpg");
+	    water.explore();
+	    water.fixUnderwater();
+	    water.explore();
+  	}
+  	
 }
